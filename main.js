ea62924917c485a18a53fa158e86336b7d37221b
pongpoti <pong.poti@gmail.com>
2026-01-03 13:34:15 +0700

    c

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 import express from "express";
 import process from "node:process";
+import path from "node:path";
 import * as line from "@line/bot-sdk";
+
+/*
+purple_200 : #e9d4ff
+pink_200 : #ffccd3
+amber_200 : #fee685
+green_200 : #b9f8cf
+*/
 
 const app = express();
 const port = process.env.PORT || 3030;
-
-app.listen(port, () => {
-  console.log("server on..");
-});
-
 const config = {
   channelSecret: "c5cefb180914e47e06498b342b77582c",
 };
 const client = new line.messagingApi.MessagingApiClient({
   channelAccessToken: "uWCHXalmoUA95FiGl298LqCvCiMrRyebRez/hbfEUiV1Xilk4ZdULAImv2vAdJRmc+v9GNyL2HXQ0gNCFBNAD3aNZpWyhAxK16sIGB/BrQ7oaSLdHjClBUFk8CgXLClQlyeRngref8TbpfBZN0JuEgdB04t89/1O/w1cDnyilFU=",
 });
+
+app.use(express.static(path.join(import.meta.dirname, 'public')));
+
+app.listen(port, () => {
+  console.log("server on..");
+});
+
+
+
 
