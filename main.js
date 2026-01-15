785ba4135dffbac1e3d64338db8f898b7adc5356
pongpoti <pong.poti@gmail.com>
2026-01-15 19:45:40 +0000

    c

--- main.js
+++ main.js
@@ -63,6 +63,7 @@ app.use("/submit", express.static("submit"))
 app.get("/insert/initiate", (req, res) => {
   console.log(req.params)
   console.log(req.query)
+  console.log(Object.values(req.query))
   //const x = req.query.liff.state.replace("?header=", "")
   //console.log(x)
   //const header = req.query.liff.state.replace("%3Fheader%3D", "")
